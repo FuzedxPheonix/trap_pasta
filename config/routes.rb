cb6c944aeb01af245386f13e47642e9b4f291e83
Cristian Guillene <cristigu@umich.edu>
2017-05-18 13:57:57 -0400

    Files has been added

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  resources :menu_items
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get '/about', to: 'static_pages#about'
+  root "menu_items#index"
+end
