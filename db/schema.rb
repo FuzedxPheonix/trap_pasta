41522a7893c21cda95e7c4b2aa4f81d812405604
Cristian Guillene <cristigu@umich.edu>
2017-05-19 12:49:27 -0400

    ssws

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170519162908) do
+ActiveRecord::Schema.define(version: 20170519164803) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 20170519162908) do
   create_table "menu_items", force: :cascade do |t|
     t.string "name"
     t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "menu_items_orders", force: :cascade do |t|
+    t.integer "menu_item_id"
+    t.integer "order_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
